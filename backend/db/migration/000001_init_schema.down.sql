dae7618f08017a31f6e3a6367907e7b18c1cc8ea
Adithya Nair <56661213+git-adithyanair@users.noreply.github.com>
2023-01-30 23:25:32 -0800

    Dropping enums. (#23)
    Co-authored-by: Adithya Nair <adithya@adithyas-mbp.lan>

--- backend/db/migration/000001_init_schema.down.sql
+++ backend/db/migration/000001_init_schema.down.sql
@@ -1,7 +1,10 @@
-DROP TABLE IF EXISTS "users";
-DROP TABLE IF EXISTS "communities";
 DROP TABLE IF EXISTS "members";
 DROP TABLE IF EXISTS "items";
 DROP TABLE IF EXISTS "stores";
 DROP TABLE IF EXISTS "requests";
 DROP TABLE IF EXISTS "errands";
+DROP TABLE IF EXISTS "communities";
+DROP TABLE IF EXISTS "users";
+
+DROP TYPE IF EXISTS "request_status";
+DROP TYPE IF EXISTS "item_quantity_type";
