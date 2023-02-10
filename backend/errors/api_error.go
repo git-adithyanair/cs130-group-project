7e1e2f63ef5da0cbc0d707cc48f5b35b063af0d2
Yili Liu <akoliu8@gmail.com>
2023-02-10 10:31:39 -0800

    add api_error file changes

--- backend/errors/api_error.go
+++ backend/errors/api_error.go
@@ -9,6 +9,8 @@ const (
 	ErrCommunityCreateFail = "ERROR_COMMUNITY_CREATE_FAILURE"
 	ErrNoCommunity         = "ERROR_NO_COMMUNITY"
 	ErrAuthFail            = "ERROR_AUTH_FAILURE"
+	ErrRequestNoItems      = "ERROR_REQUEST_NO_ITEMS"
+	ErrNoStore             = "ERROR_NO_STORE"
 )
 
 var GetErrorMessage map[string]string = map[string]string{
@@ -20,4 +22,6 @@ var GetErrorMessage map[string]string = map[string]string{
 	ErrCommunityCreateFail: "Could not create a community with provided details.",
 	ErrNoCommunity:         "Could not find this community.",
 	ErrAuthFail:            "Could not authenticate user with provided details.",
+	ErrRequestNoItems:      "Request must contain at least one item.",
+	ErrNoStore:             "Could not find this store.",
 }
