2dbdca6c068d4f848e17fc23ccdd93377bced18e
Yili Liu <akoliu8@gmail.com>
2023-02-17 12:34:51 -0800

    API for get users requests grouped by status

---
+++ backend/api/user_requests.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	db "github.com/git-adithyanair/cs130-group-project/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+	"github.com/git-adithyanair/cs130-group-project/token"
+)
+
+func (server *Server) GetUserRequest(ctx *gin.Context) {
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
+	pendingRequests := getRequestsForStatus(ctx, server, authPayload.UserID, db.RequestStatusPending)
+	inProgressRequests := getRequestsForStatus(ctx, server, authPayload.UserID, db.RequestStatusInProgress)
+	completeRequests := getRequestsForStatus(ctx, server, authPayload.UserID, db.RequestStatusCompleted)
+
+	response := userRequestsResponse{
+		Pending:    pendingRequests,
+		InProgress: inProgressRequests,
+		Complete:   completeRequests,
+	}
+
+	ctx.JSON(http.StatusOK, response)
+
+}
+
+func getRequestsForStatus(ctx *gin.Context, server *Server, userID int64, status db.RequestStatus) []db.Request {
+	arg := db.GetRequestsForUserByStatusParams{
+		UserID: userID,
+		Status: status,
+	}
+
+	requests, err := server.queries.GetRequestsForUserByStatus(ctx, arg)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			ctx.JSON(http.StatusInternalServerError, unknownErrorResponse(err))
+			return nil
+		}
+	}
+
+	return requests
+}
