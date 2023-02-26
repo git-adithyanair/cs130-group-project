2d1ab9c89d3797e1f446e444ae798cc13c750a1b
angelahuqing <angelahuqing@gmail.com>
2023-02-25 18:12:51 -0800

    Angela Addition of Item and Request card components

--- frontend/GoodGrocer/App.js
+++ frontend/GoodGrocer/App.js
@@ -1,7 +1,8 @@
-import HomeScreen from './app/screens/HomeScreen'; 
+import HomeScreen from './app/screens/HomeScreen';
 import Login from './app/screens/Login'
-import Signup from './app/screens/Signup' 
+import Signup from './app/screens/Signup'
 import AddressSignup from './app/screens/AddressSignup';
+import Testing from './app/screens/Testing';
 import LoggedInHome from './app/screens/LoggedInHome';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,9 +12,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        {/* <Stack.Screen name="Testing" component={Testing} /> */}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} /> 
+        <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="AddressSignup" component={AddressSignup} />
         <Stack.Screen name="LoggedInHome" component={LoggedInHome} />
       </Stack.Navigator>
