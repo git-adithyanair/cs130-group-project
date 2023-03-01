fc6f07a330bbdaf942af5e5e2de2b59c8f86dca1
Adithya Nair <adithyanair@ucla.edu>
2023-03-01 13:46:44 -0800

    Restructured navigation in App.js to support user token logic; loading fonts.

--- frontend/GoodGrocer/app/components/TabBar.js
+++ frontend/GoodGrocer/app/components/TabBar.js
@@ -1,78 +1,86 @@
 import React from "react";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Image, StyleSheet } from 'react-native'; 
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Buy from '../screens/Buy'; 
-import Shop from '../screens/Shop';
-import JoinCommunity from '../screens/JoinCommunity';
+import { Image, StyleSheet } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Buy from "../screens/Buy";
+import Shop from "../screens/Shop";
+import JoinCommunity from "../screens/JoinCommunity";
 import YourCommunities from "../screens/YourCommunities";
-import {Dim, Colors} from "../Constants"
+import { Dim, Colors } from "../Constants";
 
 const HomeStack = createStackNavigator();
 
 const HomeStackScreen = () => {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="YourCommunities" component={YourCommunities} options={{title: "Your Communities"}}/>
-      <HomeStack.Screen name="JoinCommunity" component={JoinCommunity} options={{title: "Join Community"}}/>
+      <HomeStack.Screen
+        name="YourCommunities"
+        component={YourCommunities}
+        options={{ title: "Your Communities" }}
+      />
+      <HomeStack.Screen
+        name="JoinCommunity"
+        component={JoinCommunity}
+        options={{ title: "Join Community" }}
+      />
     </HomeStack.Navigator>
   );
-}
+};
 
-const Tab = createBottomTabNavigator(); 
+const Tab = createBottomTabNavigator();
 
 const tabBarPages = [
-    {"name": "Shop",
-    "component": Shop},
-    {"name": "Home",
-    "component": HomeStackScreen},
-    {"name": "Buy",
-    "component": Buy}
-]
+  { name: "Shop", component: Shop },
+  { name: "Home", component: HomeStackScreen },
+  { name: "Buy", component: Buy },
+];
 
 const TabBar = (props) => {
-    const components = tabBarPages.map((page, index) => <Tab.Screen name={page.name} component={page.component} key={index}/>)
-    if(components.length === 0){
-        return null; 
-    }
-    return  <Tab.Navigator screenOptions={({ route }) => ({
+  const components = tabBarPages.map((page, index) => (
+    <Tab.Screen name={page.name} component={page.component} key={index} />
+  ));
+  if (components.length === 0) {
+    return null;
+  }
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarShowLabel: false,
         tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === 'Shop') {
-              iconName = focused
-                ? 'ios-cart'
-                : 'ios-cart-outline';
-            } else if (route.name === 'Buy') {
-              iconName = focused ? 'ios-pizza' : 'ios-pizza-outline';
-            }
-            else if (route.name === 'Home') {
-                return <Image source={{uri: props.imageUri}} style={styles.logo} />;
-            }
-            return <Ionicons name={iconName} size={size} color={'white'} />;
-          },
+          let iconName;
+          if (route.name === "Shop") {
+            iconName = focused ? "ios-cart" : "ios-cart-outline";
+          } else if (route.name === "Buy") {
+            iconName = focused ? "ios-pizza" : "ios-pizza-outline";
+          } else if (route.name === "Home") {
+            return (
+              <Image source={{ uri: props.imageUri }} style={styles.logo} />
+            );
+          }
+          return <Ionicons name={iconName} size={size} color={"white"} />;
+        },
         tabBarStyle: {
           height: Dim.width * 0.15,
-          width: Dim.width, 
+          width: Dim.width,
           backgroundColor: Colors.darkGreen,
           color: Colors.white,
-          position: 'absolute',
-          marginBottom: 0
-      },
-    })}>
-        {components}
-    </Tab.Navigator>; 
+          position: "absolute",
+          marginBottom: 0,
+        },
+      })}
+    >
+      {components}
+    </Tab.Navigator>
+  );
 };
 
-
 const styles = StyleSheet.create({
-    logo: {
-      width: 35,
-      height: 35
-    },
-  });
-
+  logo: {
+    width: 35,
+    height: 35,
+  },
+});
 
 export default TabBar;
--- frontend/GoodGrocer/app/screens/Signup.js
+++ frontend/GoodGrocer/app/screens/Signup.js
@@ -1,48 +1,69 @@
-import React, {useState}  from 'react';
-import TextInput from '../components/TextInput';
-import Button from '../components/Button';
-import { SafeAreaView, StyleSheet, Text, Image, View, Pressable } from 'react-native';
+import React, { useState } from "react";
+import TextInput from "../components/TextInput";
+import Button from "../components/Button";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  Pressable,
+} from "react-native";
 
-function Signup({navigation}) {
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [name, setName] = useState(''); 
-    const [password, setPassword] = useState(''); 
+function Signup({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-   
-    return (
-        <SafeAreaView style={styles.container}>
-        <View> 
-        <Image source={require("../assets/logo.png")}/>
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Image source={require("../assets/logo.png")} />
         <Text style={styles.titleText}>Signup</Text>
         <Text>Email</Text>
-        <TextInput onChange={email=>setEmail(email.nativeEvent.text)}/>
+        <TextInput onChange={(email) => setEmail(email.nativeEvent.text)} />
         <Text>Phone Number</Text>
-        <TextInput onChange={phoneNumber=>setPhoneNumber(phoneNumber.nativeEvent.text)}/>
+        <TextInput
+          onChange={(phoneNumber) =>
+            setPhoneNumber(phoneNumber.nativeEvent.text)
+          }
+        />
         <Text>Name</Text>
-        <TextInput onChange={name=>setName(name.nativeEvent.text)}/>
+        <TextInput onChange={(name) => setName(name.nativeEvent.text)} />
         <Text>Password</Text>
-        <TextInput onChange={password=>setPassword(password.nativeEvent.text)}/>
-        <Button title={"Continue with Address"} onPress={() => navigation.navigate('AddressSignup', {email, phoneNumber, name, password})} textColor={"white"} backgroundColor={"#0070CA"} width={300} />
-        </View>
-        </SafeAreaView>
-    );
-
+        <TextInput
+          onChange={(password) => setPassword(password.nativeEvent.text)}
+        />
+        <Button
+          title={"Continue with Address"}
+          onPress={() =>
+            navigation.navigate("AddressSignup", {
+              email,
+              phoneNumber,
+              name,
+              password,
+            })
+          }
+          textColor={"white"}
+          backgroundColor={"#0070CA"}
+          width={300}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
   titleText: {
-    fontSize: 25
-  }
+    fontSize: 25,
+  },
 });
-
 
 export default Signup;
