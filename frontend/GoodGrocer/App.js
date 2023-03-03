31f2edb0f959eb079f55c227d6041c3fcc475dd9
angelahuqing <angelahuqing@gmail.com>
2023-03-03 15:11:17 -0800

    remove testing

--- frontend/GoodGrocer/App.js
+++ frontend/GoodGrocer/App.js
@@ -4,7 +4,6 @@ import HomeScreen from './app/screens/HomeScreen';
 import Login from './app/screens/Login'
 import Signup from './app/screens/Signup'
 import AddressSignup from './app/screens/AddressSignup';
-import Testing from './app/screens/Testing';
 import LoggedInHome from './app/screens/LoggedInHome';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -14,7 +13,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="Testing" component={Testing} /> */}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
