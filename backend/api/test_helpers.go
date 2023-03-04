17be59ecd2bcdd5af1a72fc73740d929a9185c94
Adithya Nair <adithyanair@ucla.edu>
2023-03-03 17:42:56 -0800

    Added unit tests for request creation API.

--- backend/api/test_helpers.go
+++ backend/api/test_helpers.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -20,6 +21,7 @@ func createRandomUser(t *testing.T) (db.User, string) {
 	hashedPassword, err := util.HashPassword(rawPassword)
 	require.NoError(t, err)
 	return db.User{
+		ID:             util.RandomID(),
 		Email:          util.RandomEmail(),
 		HashedPassword: hashedPassword,
 		FullName:       util.RandomFullName(),
@@ -49,6 +51,7 @@ func requireBodyMatchUser(t *testing.T, body *bytes.Buffer, user db.User) {
 
 func createRandomCommunity(t *testing.T, adminID int64) db.Community {
 	return db.Community{
+		ID:           util.RandomID(),
 		Name:         util.RandomCommunityName(),
 		Admin:        adminID,
 		PlaceID:      util.RandomPlaceID(),
@@ -56,6 +59,72 @@ func createRandomCommunity(t *testing.T, adminID int64) db.Community {
 		CenterYCoord: util.RandomCoordinate(),
 		Address:      util.RandomAddress(),
 		Range:        util.RandomRange(),
+	}
+}
+
+func createRandomStore(t *testing.T) db.Store {
+	return db.Store{
+		ID:      util.RandomID(),
+		Name:    util.RandomStoreName(),
+		PlaceID: util.RandomPlaceID(),
+		XCoord:  util.RandomCoordinate(),
+		YCoord:  util.RandomCoordinate(),
+		Address: util.RandomAddress(),
+	}
+}
+
+func createRandomCommunityStore(t *testing.T, communityID int64, storeID int64) db.CommunityStore {
+	return db.CommunityStore{
+		CommunityID: communityID,
+		StoreID:     storeID,
+	}
+}
+
+func createRandomRequest(t *testing.T, userID int64, communityID int64, storeID int64) db.Request {
+	return db.Request{
+		ID:          util.RandomID(),
+		UserID:      userID,
+		CommunityID: sql.NullInt64{Int64: communityID, Valid: true},
+		StoreID:     sql.NullInt64{Int64: storeID, Valid: true},
+	}
+}
+
+func createRandomItem(
+	t *testing.T,
+	requestedBy int64,
+	requestID int64,
+	quantityType db.ItemQuantityType,
+	quantity float64,
+	withPreferredBrand bool,
+	withImage bool,
+	withExtraNotes bool,
+) db.Item {
+	preferredBrand := sql.NullString{String: "", Valid: false}
+	if withPreferredBrand {
+		preferredBrand.Valid = true
+		preferredBrand.String = util.RandomString(6)
+	}
+	image := sql.NullString{String: "", Valid: false}
+	if withImage {
+		image.Valid = true
+		image.String = util.RandomString(300)
+	}
+	extraNotes := sql.NullString{String: "", Valid: false}
+	if withExtraNotes {
+		extraNotes.Valid = true
+		extraNotes.String = util.RandomString(20)
+	}
+	return db.Item{
+		ID:             util.RandomID(),
+		Name:           util.RandomItemName(),
+		RequestedBy:    requestedBy,
+		RequestID:      requestID,
+		QuantityType:   quantityType,
+		Quantity:       quantity,
+		PreferredBrand: preferredBrand,
+		Image:          image,
+		Found:          sql.NullBool{Bool: false, Valid: false},
+		ExtraNotes:     extraNotes,
 	}
 }
 
