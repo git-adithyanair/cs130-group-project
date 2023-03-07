02cca3c4fa5fc929e81407341613c024e6b02380
Yili Liu <akoliu8@gmail.com>
2023-03-07 12:41:01 -0800

    Add item and store info in user requests API

--- backend/api/user_requests_test.go
+++ backend/api/user_requests_test.go
@@ -9,6 +9,7 @@ import (
 	mock_db "github.com/git-adithyanair/cs130-group-project/db/mock"
 	db "github.com/git-adithyanair/cs130-group-project/db/sqlc"
 	"github.com/git-adithyanair/cs130-group-project/token"
+	"github.com/git-adithyanair/cs130-group-project/util"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
@@ -17,15 +18,24 @@ func TestGetUserRequest(t *testing.T) {
 
 	user, _ := createRandomUser(t)
 	in_progress, complete, pending := []db.Request{}, []db.Request{}, []db.Request{}
+	groceryStore := createRandomStore(t)
+	items := make(map[int64]([]db.Item))
+	requests_no_items := []db.Request{createRandomRequest(t, user.ID, util.RandomID(), groceryStore.ID)}
+	requests_invalid_store := []db.Request{createRandomRequest(t, user.ID, util.RandomID(), groceryStore.ID+100)}
+	requests_invalid_store_items := []db.Item{createRandomItem(t, user.ID, requests_invalid_store[0].ID, db.ItemQuantityTypeGal, 0.0, false, false, false)}
 
 	for i := 0; i < 10; i++ {
-		request := createRandomRequestWithRandomStatus(t, user.ID)
+		request := createRandomRequestWithRandomStatus(t, user.ID, groceryStore.ID)
 		if request.Status == db.RequestStatusInProgress {
 			in_progress = append(in_progress, request)
 		} else if request.Status == db.RequestStatusPending {
 			pending = append(pending, request)
 		} else if request.Status == db.RequestStatusCompleted {
 			complete = append(complete, request)
+		}
+		items[request.ID] = []db.Item{
+			createRandomItem(t, user.ID, request.ID, db.ItemQuantityTypeGal, 0.0, false, false, false),
+			createRandomItem(t, user.ID, request.ID, db.ItemQuantityTypeGal, 0.0, false, false, false),
 		}
 	}
 
@@ -53,6 +63,16 @@ func TestGetUserRequest(t *testing.T) {
 					}).
 					Times(1).
 					Return(pending, nil)
+				for _, request := range pending {
+					store.EXPECT().
+						GetItemsByRequest(gomock.Any(), request.ID).
+						Times(1).
+						Return(items[request.ID], nil)
+					store.EXPECT().
+						GetStore(gomock.Any(), groceryStore.ID).
+						Times(1).
+						Return(groceryStore, nil)
+				}
 				store.EXPECT().
 					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
 						UserID: user.ID,
@@ -60,6 +80,16 @@ func TestGetUserRequest(t *testing.T) {
 					}).
 					Times(1).
 					Return(in_progress, nil)
+				for _, request := range in_progress {
+					store.EXPECT().
+						GetItemsByRequest(gomock.Any(), request.ID).
+						Times(1).
+						Return(items[request.ID], nil)
+					store.EXPECT().
+						GetStore(gomock.Any(), groceryStore.ID).
+						Times(1).
+						Return(groceryStore, nil)
+				}
 				store.EXPECT().
 					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
 						UserID: user.ID,
@@ -67,6 +97,16 @@ func TestGetUserRequest(t *testing.T) {
 					}).
 					Times(1).
 					Return(complete, nil)
+				for _, request := range complete {
+					store.EXPECT().
+						GetItemsByRequest(gomock.Any(), request.ID).
+						Times(1).
+						Return(items[request.ID], nil)
+					store.EXPECT().
+						GetStore(gomock.Any(), groceryStore.ID).
+						Times(1).
+						Return(groceryStore, nil)
+				}
 			},
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusOK, recorder.Code)
@@ -162,6 +202,16 @@ func TestGetUserRequest(t *testing.T) {
 					}).
 					Times(1).
 					Return(pending, nil)
+				for _, request := range pending {
+					store.EXPECT().
+						GetItemsByRequest(gomock.Any(), request.ID).
+						Times(1).
+						Return(items[request.ID], nil)
+					store.EXPECT().
+						GetStore(gomock.Any(), groceryStore.ID).
+						Times(1).
+						Return(groceryStore, nil)
+				}
 				store.EXPECT().
 					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
 						UserID: user.ID,
@@ -169,6 +219,16 @@ func TestGetUserRequest(t *testing.T) {
 					}).
 					Times(1).
 					Return(in_progress, nil)
+				for _, request := range in_progress {
+					store.EXPECT().
+						GetItemsByRequest(gomock.Any(), request.ID).
+						Times(1).
+						Return(items[request.ID], nil)
+					store.EXPECT().
+						GetStore(gomock.Any(), groceryStore.ID).
+						Times(1).
+						Return(groceryStore, nil)
+				}
 				store.EXPECT().
 					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
 						UserID: user.ID,
@@ -176,6 +236,102 @@ func TestGetUserRequest(t *testing.T) {
 					}).
 					Times(1).
 					Return([]db.Request{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "StatusNotFoundItem",
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, user.Email)
+			},
+			buildStubs: func(store *mock_db.MockDBStore) {
+				store.EXPECT().
+					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
+						UserID: user.ID,
+						Status: db.RequestStatusPending,
+					}).
+					Times(1).
+					Return(requests_no_items, nil)
+				store.EXPECT().
+					GetItemsByRequest(gomock.Any(), requests_no_items[0].ID).
+					Times(1).
+					Return([]db.Item{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name: "InternalServerErrorItem",
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, user.Email)
+			},
+			buildStubs: func(store *mock_db.MockDBStore) {
+				store.EXPECT().
+					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
+						UserID: user.ID,
+						Status: db.RequestStatusPending,
+					}).
+					Times(1).
+					Return(requests_no_items, nil)
+				store.EXPECT().
+					GetItemsByRequest(gomock.Any(), requests_no_items[0].ID).
+					Times(1).
+					Return([]db.Item{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "StatusNotFoundStore",
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, user.Email)
+			},
+			buildStubs: func(store *mock_db.MockDBStore) {
+				store.EXPECT().
+					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
+						UserID: user.ID,
+						Status: db.RequestStatusPending,
+					}).
+					Times(1).
+					Return(requests_invalid_store, nil)
+				store.EXPECT().
+					GetItemsByRequest(gomock.Any(), requests_invalid_store[0].ID).
+					Times(1).
+					Return(requests_invalid_store_items, nil)
+				store.EXPECT().
+					GetStore(gomock.Any(), groceryStore.ID+100).
+					Times(1).
+					Return(db.Store{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name: "InternalServerErrorStore",
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.ID, user.Email)
+			},
+			buildStubs: func(store *mock_db.MockDBStore) {
+				store.EXPECT().
+					GetRequestsForUserByStatus(gomock.Any(), db.GetRequestsForUserByStatusParams{
+						UserID: user.ID,
+						Status: db.RequestStatusPending,
+					}).
+					Times(1).
+					Return(requests_invalid_store, nil)
+				store.EXPECT().
+					GetItemsByRequest(gomock.Any(), requests_invalid_store[0].ID).
+					Times(1).
+					Return(requests_invalid_store_items, nil)
+				store.EXPECT().
+					GetStore(gomock.Any(), groceryStore.ID+100).
+					Times(1).
+					Return(db.Store{}, sql.ErrConnDone)
 			},
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 				require.Equal(t, http.StatusInternalServerError, recorder.Code)
