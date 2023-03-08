9d5c196d1b56cb6469fd5bb42e983038e7236894
Adithya Nair <adithyanair@ucla.edu>
2023-03-08 00:33:56 -0800

    Added unit testing setup with Jest.

---
+++ frontend/GoodGrocer/test/Login.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render, fireEvent } from "@testing-library/react-native";
+import { Provider } from "react-redux";
+
+import { store } from "../app/store/config";
+import { setToken } from "../app/store/actions";
+import useRequest from "../app/hooks/useRequest";
+import Login from "../app/screens/Login";
+
+test("given valid email and password, the login is successful", () => {
+  const component = render(
+    <Provider store={store}>
+      <Login />
+    </Provider>
+  );
+
+  const mockedDoRequest = jest.fn(() => {
+    store.dispatch(setToken("token"));
+    console.log("HERE");
+  });
+
+  useRequest.mockImplementation(() => {
+    return {
+      doRequest: mockedDoRequest,
+    };
+  });
+
+  const { doRequest } = useRequest();
+
+  fireEvent.changeText(
+    component.getByPlaceholderText("Enter your email..."),
+    "email"
+  );
+  fireEvent.changeText(
+    component.getByPlaceholderText("Enter your password..."),
+    "password"
+  );
+
+  fireEvent.press(component.queryByText("Log In"));
+
+  expect(doRequest).toHaveBeenCalled();
+  expect(store.getState().user.token).toBe("token");
+});
